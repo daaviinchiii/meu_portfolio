50b3b9c8be2af4514b2a5daedbbf07fec9eb7baa
daaviinchiii <dvdcardoso23@gmail.com>
2023-07-13 23:29:53 +0000

    final

--- src/components/about_section/About.jsx
+++ src/components/about_section/About.jsx
@@ -32,7 +32,7 @@ const AboutSec = () => {
             {/* button */}
             <div className="mt-7">
               <a
-                href="https://daaviinchiii.github.io/about"
+                href="./about"
                 className=" duration-150 hover:bg-[#C778DD33] border border-[#C778DD] px-4 py-2 text-white "
               >
                 <button>Saiba Mais -{">"}</button>
